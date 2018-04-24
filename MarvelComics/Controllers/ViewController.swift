c682c50c07c5b3715bd89d36153b8b893e40276a
Jeremy Broutin <jeremybroutin@google.com>
2018-04-24 15:55:54 -0700

    Adds spike code in view controller to display results

--- MarvelComics/Controllers/ViewController.swift
+++ MarvelComics/Controllers/ViewController.swift
@@ -2,22 +2,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  var tableView: UITableView!
+  var service: FetchCharactersMarvelService!
+  var results: [CharacterResponseModel]!
+
+  let cellID = "ResultCell"
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    let service = FetchCharactersMarvelService(session: URLSession.shared) { () -> String in
+    service = FetchCharactersMarvelService(session: URLSession.shared) {
       return MarvelAuthentication().urlParameters()
     }
-    let requestModel = FetchCharactersRequestModel(namePrefix: "Spider", limit: 10, offset: 0)
+
+    setupUI()
+
+    // Test
+    fetchCharacters(for: "Spider")
+  }
+
+  private func setupUI() {
+    let statusBarHeight = UIApplication.shared.statusBarFrame.height
+    let viewWidth = self.view.frame.width
+    let viewHeight = self.view.frame.height
+    let frame = CGRect(x: 0, y: statusBarHeight,
+                       width: viewWidth, height: viewHeight - statusBarHeight)
+    setupTableView(frame: frame)
+  }
+
+  private func setupTableView(frame: CGRect) {
+    tableView = UITableView(frame: frame, style: .plain)
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+    tableView.delegate = self
+    tableView.dataSource = self
+    self.view.addSubview(tableView)
+  }
+
+
+  func fetchCharacters(for prefix: String) {
+    let requestModel = FetchCharactersRequestModel(namePrefix: prefix, limit: 10, offset: 0)
     service.fetchCharacters(requestModel: requestModel, networkRequest: NetworkRequest()) {
       responseModel in
       switch responseModel {
       case let .success(charactersSlice):
-        print("success: \(charactersSlice)")
+        self.results = charactersSlice.characters
+        DispatchQueue.main.async {
+          self.tableView.reloadData()
+        }
       case let .failure(error):
         print("error: \(error)")
       }
     }
+  }
+
+}
+
+extension ViewController: UITableViewDelegate {
+
+}
+
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return results?.count ?? 0
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+    cell.textLabel?.text = results?[indexPath.row].name
+    return cell
   }
 }
 
